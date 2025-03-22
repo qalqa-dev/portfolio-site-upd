c56185b98e03492b4351d41e300935362dd58f13
qalqa-dev <andreybas04@gmail.com>
2025-03-22 03:38:43 +0300

    feat: added clicker +  pickaxe handle

--- src/pages/Craft/Craft.tsx
+++ src/pages/Craft/Craft.tsx
@@ -19,8 +19,57 @@ const Craft = () => {
     Array.from({ length: 4 }, () => Array.from({ length: 12 }, () => ({}))),
   );
 
+  const [pickaxe, setPickaxe] = useState<string>();
+
+  const [backgroundSize, setBackgroundSize] = useState(100);
+
+  const handleClick = () => {
+    setBackgroundSize(70);
+    setTimeout(() => {
+      setBackgroundSize(100);
+    }, 300);
+  };
+
   return (
     <div className={styles.container}>
+      <Safari openedLink="qalqa.com/mine">
+        <div className={styles.webview}>
+          <div className={styles.clicker}>
+            <h2 className={styles.title}>
+              <AppearingText text="Mine" />
+            </h2>
+            <div className="grid grid-cols-[1fr_3fr_1fr] w-full">
+              <div></div>
+              <div onClick={handleClick} className={styles['clicker-block']}>
+                <div
+                  className={styles['clicker-backdrop']}
+                  style={{
+                    backgroundSize: `${backgroundSize}%`,
+                  }}
+                >
+                  <img
+                    width={275}
+                    height={275}
+                    src="/src/assets/clicker_blocks/wood.png"
+                    alt="block"
+                  />
+                </div>
+              </div>
+              <div className={styles['clicker-pickaxe']}>
+                <div className={styles['clicker-pickaxe-container']}>
+                  {pickaxe && (
+                    <img
+                      className={styles['clicker-pickaxe-img']}
+                      src={`/src/assets/clicker_tools/${pickaxe}_pickaxe.webp`}
+                      alt=""
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Safari>
       <Safari openedLink="qalqa.com/craft">
         <div className={styles.webview}>
           <div className="flex justify-center">
